9de6f0231afeb2978ca7274471e55ffabbb3c040
idoncev <ivan.doncevic@rtl.hr>
2021-03-29 17:18:39 +0200

    parse folder for svg files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Resizes and centers any svg to specified viewport (default is 0 0 32 32)
 
-Usage:
+# Usage:
 
 ```
 node transform-svg.js "svg-name.svg" --viewport="0 0 32 32"
@@ -8,3 +8,13 @@ node transform-svg.js "svg-name.svg" --viewport="0 0 32 32"
 
 node transform-svg.js /home/svgs-to-convert --outFolder=/home/converted --toJsTemplate --viewport="0 0 32 32"
 ```
+
+# Params:
+
+- first anonymous argument - either file or directory with svg files to process
+- --file - specific file to convert, takes priority over --directory
+- --directory - specify folder that contains svgs to convert
+- --toJsTemplate - create js template files for human Icon component - default: true
+  - this command will also strip colors and all other unneccessary attributes from svg paths
+- --outFolder - specify folder where to output files - default is either folder of the converted file, or sibling directory (+converted) of selected dir
+- --viewport - specify new viewport for svg file - default: 0 0 32 32
--- transform-svg.js
+++ transform-svg.js
@@ -1,23 +1,27 @@
 #!/usr/bin/env node
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
+const fs = require("fs")
+const path = require('path')
 const argv = yargs(hideBin(process.argv)).argv
-
-console.log(argv)
 
 const checkParam = (param) => param!==undefined && param !==null
 
-
 const settings = {
-    stripColors : checkParam(argv.stripColors) ? Boolean(argv.stripColors) : true,
     targetViewport : checkParam(argv.viewport) ? argv.viewport : '0 0 32 32',
-    target : checkParam(argv._[0]) ? argv._[0]: checkParam(argv.file) ? argv.file : checkParam(argv.directory)? argv.directory : undefined,
+    target : path.normalize(checkParam(argv.file) ? argv.file : checkParam(argv.directory)? argv.directory : checkParam(argv._[0]) ? argv._[0]:  undefined),
     createJsTemplate : checkParam(argv.toJsTemplate)? Boolean(argv.toJsTemplate) : true,
     saveTransformedSvg : checkParam(argv.saveSvg) ? Boolean(argv.saveSvg) : false,
     outputFolder: checkParam(argv.outFolder) ? argv.outFolder : __dirname 
 }
+console.log(settings)
 
 if(!settings.target) throw `target file or folder not specified`
+if (!fs.existsSync(settings.target)) throw 'target file or directory doesn\'t exist' 
 
 
-console.log(settings)
+if(fs.lstatSync(settings.target).isDirectory() ){
+    console.log("target is folder")
+    const files = fs.readdirSync(settings.target).filter(file => file.match(/.svg$/gim));
+    console.log("svg files",files)
+}
