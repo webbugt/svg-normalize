bd0cad121648976f4838213f430b830868eb01be
idoncev <ivan.doncevic@rtl.hr>
2021-03-29 16:51:16 +0200

    implement yargs

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Resizes and centers any svg to specified viewport (default is 0 0 32 32)
 Usage:
 
 ```
-node transform-svg.js --file="svg-name.svg" --viewport="0 0 32 32"
+node transform-svg.js "svg-name.svg" --viewport="0 0 32 32"
+
+
+node transform-svg.js /home/svgs-to-convert --outFolder=/home/converted --toJsTemplate --viewport="0 0 32 32"
 ```
--- transform-svg.js
+++ transform-svg.js
@@ -2,3 +2,22 @@
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
+
+console.log(argv)
+
+const checkParam = (param) => param!==undefined && param !==null
+
+
+const settings = {
+    stripColors : checkParam(argv.stripColors) ? Boolean(argv.stripColors) : true,
+    targetViewport : checkParam(argv.viewport) ? argv.viewport : '0 0 32 32',
+    target : checkParam(argv._[0]) ? argv._[0]: checkParam(argv.file) ? argv.file : checkParam(argv.directory)? argv.directory : undefined,
+    createJsTemplate : checkParam(argv.toJsTemplate)? Boolean(argv.toJsTemplate) : true,
+    saveTransformedSvg : checkParam(argv.saveSvg) ? Boolean(argv.saveSvg) : false,
+    outputFolder: checkParam(argv.outFolder) ? argv.outFolder : __dirname 
+}
+
+if(!settings.target) throw `target file or folder not specified`
+
+
+console.log(settings)
